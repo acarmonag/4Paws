e55a8d4f236ad6a320e0bc529c3474fcd43e8025
acarmonag <acarmonag@eafit.edu.co>
2022-08-03 00:32:50 +0000

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page</h1>')
-    return render(request, 'home.html',{'name':'Antonio'})
+    return render(request, 'home.html',{'name':'Antonio Carmona'})
